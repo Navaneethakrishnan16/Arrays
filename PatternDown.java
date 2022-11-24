efe438cf1d31d8d5ad7958cd64e1ebf4d6bf6bbd
Navaneethakrishnan16 <117495891+Navaneethakrishnan16@users.noreply.github.com>
2022-11-24 15:22:42 +0530

    Add files via upload

---
+++ PatternDown.java
@@ -0,0 +1,16 @@
+package patterndown;
+
+public class PatternDown {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+              int i,j;
+              for(i=5;i>=0;i--) {
+            	  for( j=1;j<=i;j++) {
+            		  System.out.print("*");
+            	  }
+            	  System.out.println();
+              }
+	}
+
+}
